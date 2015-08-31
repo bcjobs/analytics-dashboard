234900e2634a8581e9c5a9e6f11888c2a1654d82
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2015-08-31 22:45:44 +0200

    add server with api proxy

---
+++ web.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var httpProxy = require('http-proxy');
+var path = require('path');
+
+var app = express();
+var proxy = httpProxy.createProxyServer({changeOrigin: true});
+
+app.use('/api', function(req, res, next) {
+  proxy.web(req, res, { target: 'http://analytics.bcjobs.ca/api' });
+});
+
+app.use('/assets', express.static(__dirname + '/dist/assets'));
+app.use('/images', express.static(__dirname + '/dist/images'));
+
+app.use(function(req, res, next) {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+var port = process.env.PORT || 4200;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
