b2dbea35f8031d170837eb8b7885e9a8367419b8
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2015-09-01 11:37:36 +0200

    show direction icons based on current vs. average value

--- app/components/bcic-report/component.js
+++ app/components/bcic-report/component.js
@@ -1,7 +1,33 @@
 import Ember from 'ember';
 import range from 'npm:lodash/utility/range';
 
+function direction(current, average) {
+  if (current > average) {
+    return 'up';
+  }
+  else if (current < average) {
+    return 'down';
+  }
+  else {
+    return null;
+  }
+}
+
 export default Ember.Component.extend({
+  openJobsDirection: Ember.computed('model.summary.openJobs.current', 'model.summary.openJobsAverage.current', function() {
+    return direction(
+      this.get('model.summary.openJobs.current'),
+      this.get('model.summary.openJobsAverage.current')
+    );
+  }),
+
+  newJobsDirection: Ember.computed('model.summary.newJobs.current', 'model.summary.newJobsAverage.current', function() {
+    return direction(
+      this.get('model.summary.newJobs.current'),
+      this.get('model.summary.newJobsAverage.current')
+    );
+  }),
+
   five: range(0, 5),
   ten: range(0, 10)
 });
--- app/components/overview-stats/component.js
+++ app/components/overview-stats/component.js
@@ -1,18 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  direction: Ember.computed('model.current', 'model.previous', function() {
-    var current = this.get('model.current');
-    var previous = this.get('model.previous');
-
-    if (current > previous) {
-      return 'up';
-    }
-    else if (previous > current) {
-      return 'down';
-    }
-    else {
-      return null;
-    }
-  })
 });
