ca2bcdbee814bfd83c1598ac40c1ed3c06db61b0
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2015-09-01 18:32:28 +0200

    pass all query parameters to API

--- app/index/route.js
+++ app/index/route.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return Ember.RSVP.resolve(
-      Ember.$.getJSON('/api/v1.0/reports/outline')
+      Ember.$.getJSON('/api/v1.0/reports/outline' + window.location.search)
     );
   }
 });
