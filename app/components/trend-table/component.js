0b32b74a9ec19d9c89163de169cd1620d0c72a44
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2015-10-17 15:03:53 +0200

    make list expandable to more items

--- app/components/trend-table/component.js
+++ app/components/trend-table/component.js
@@ -1,4 +1,28 @@
 import Ember from 'ember';
 
+var DEFAULT_ITEMS = 5;
+
 export default Ember.Component.extend({
+  isShowingAll: false,
+
+  showMoreButton: Ember.computed('isShowingAll', 'model', function() {
+    if (this.get('isShowingAll')) {
+      return false;
+    }
+    return this.get('model').length > DEFAULT_ITEMS;
+  }),
+
+  items: Ember.computed('isShowingAll', 'model', function() {
+    var model = this.get('model') || [];
+    if (this.get('isShowingAll')) {
+      return model;
+    }
+    return model.slice(0, DEFAULT_ITEMS);
+  }),
+
+  actions: {
+    showMore() {
+      this.set('isShowingAll', true);
+    }
+  }
 });
