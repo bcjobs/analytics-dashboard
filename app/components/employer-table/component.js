a79da00d22438dda8a9ec21980910bb9b0c9d768
Johnny Oshika <johnnyoshika@gmail.com>
2015-10-28 11:10:55 -0700

    Added 'See More' to employer tables.

--- app/components/employer-table/component.js
+++ app/components/employer-table/component.js
@@ -1,9 +1,26 @@
 import Ember from 'ember';
 import range from 'npm:lodash/utility/range';
 
-var DEFAULT_ITEMS = 10;
+var DEFAULT_ITEMS = 5;
 
 export default Ember.Component.extend({
+  isShowingAll: false,
+
+  showMoreButton: Ember.computed('isShowingAll', 'model', function() {
+    if (this.get('isShowingAll')) {
+      return false;
+    }
+    return this.get('model.top').length > DEFAULT_ITEMS;
+  }),
+
+  items: Ember.computed('isShowingAll', 'model', function() {
+    var model = this.get('model.top') || [];
+    if (this.get('isShowingAll')) {
+      return model;
+    }
+    return model.slice(0, DEFAULT_ITEMS);
+  }),
+
   emptyItems: Ember.computed('model.top', function() {
     var length = this.get('model.top.length');
     if (length >= DEFAULT_ITEMS) {
@@ -11,4 +28,10 @@ export default Ember.Component.extend({
     }
     return range(0,  DEFAULT_ITEMS - length);
   }),
+
+  actions: {
+    showMore() {
+      this.set('isShowingAll', true);
+    }
+  }
 });
