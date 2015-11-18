16dcd27989027fe9a1b11fc065f2e2f1d372ea05
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2015-11-18 14:45:44 +0100

    show mock rules

--- app/admin/rules/route.js
+++ app/admin/rules/route.js
@@ -3,12 +3,30 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
-      ruleTypes: ["Job Title", "Company Name"],
-      ruleType: "Job Title"
+      ruleTypes: ['Job Title', 'Company Name'],
+      ruleType: 'Job Title',
+      rules: [{
+        isNew: true,
+        terms: ['']
+      }]
     })
   },
 
   setupController(controller, models) {
     controller.setProperties(models);
+  },
+
+  actions: {
+    search(text) {
+      this.controller.set('rules', [{
+        terms: ['Sr. Developer', 'Developer II'],
+    		action: 'replace',
+    		replaceWith: 'Senior Developer'
+      },{
+        terms: ['C# Developer'],
+    		action: 'replace',
+    		replaceWith: '.NET Developer'
+      }]);
+    }
   }
 });
