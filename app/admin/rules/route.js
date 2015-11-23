ccbbe1248fff0b092436f783a6f6d154683f3551
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2015-11-23 20:22:36 +0100

    add refreshmodel to ruleType query param

--- app/admin/rules/route.js
+++ app/admin/rules/route.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  queryParams: {
+    ruleType: {
+      refreshModel: true
+    }
+  },
+
   model() {
     return ["Job Title", "Company Name"];
   },
