8853ca9b8bb079e96e6d173604e574ebb6f0c182
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2015-11-30 17:26:38 +0100

    redirect to login screen on each 401 response

--- app/admin/route.js
+++ app/admin/route.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    error(err) {
+      console.log(err);
+      if (err.status === 401) {
+        this.transitionTo('authentication');
+      }
+    }
+  }
 });
--- server/mocks/rule-types.js
+++ server/mocks/rule-types.js
@@ -3,6 +3,7 @@ module.exports = function(app) {
   var ruleTypesRouter = express.Router();
 
   ruleTypesRouter.get('/', function(req, res) {
+    // res.status(401).send({message: 'Not logged in', code: 'not_logged'});
     res.send(['Job Title', 'Company Name', 'Skill']);
   });
 
