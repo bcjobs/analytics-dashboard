a8b2a69293370db54d919381a45c7860cd7e07d5
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2015-11-30 17:34:06 +0100

    login redirects directly to rules
    for now, until we have more admin screens

--- app/authentication/login/route.js
+++ app/authentication/login/route.js
@@ -9,7 +9,7 @@ export default Ember.Route.extend({
         method: 'POST',
         data: {email, password}
       }).then(() => {
-        this.transitionTo('admin');
+        this.transitionTo('admin.rules');
       }).catch(err => {
         var errorMessage = Ember.get(err, 'responseJSON.message') || 'Internal Error';
         this.controller.set('errorMessage', errorMessage);
