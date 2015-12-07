a4c26e09844a7d40376b5befd08a5b975efa377d
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2015-12-07 15:40:22 +0100

    set password
    fixes #12

--- app/authentication/password/route.js
+++ app/authentication/password/route.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    setPassword() {
+      var data = this.controller.getProperties('password', 'token', 'id');
+      this.store.ajax({
+        url: '/api/v1.0/authentication/password',
+        method: 'PUT',
+        data: data
+      }).then(() => {
+        this.controller.set('done', true);
+      }).catch(err => {
+        var errorMessage = Ember.get(err, 'responseJSON.message') || 'Internal Error';
+        this.controller.set('errorMessage', errorMessage);
+      });
+    }
+  }
 });
