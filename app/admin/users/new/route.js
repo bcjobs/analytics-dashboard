bc5e438c43adac92b2042a43cba006263ac4a42a
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2015-12-07 15:11:58 +0100

    add user
    relates to #11

--- app/admin/users/new/route.js
+++ app/admin/users/new/route.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return {
+      email: '',
+      firstName: '',
+      lastName: ''
+    };
+  },
+
+  actions: {
+    save() {
+      var user = this.controller.get('model');
+      this.store.ajax({
+        url: `/api/v1.0/users`,
+        method: 'POST',
+        data: user
+      }).then(() => {
+        this.transitionTo('admin.users');
+      });
+    }
+  }
 });
