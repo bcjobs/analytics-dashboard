66c8c2dfae0292ce3f7d0487ccab3921c1ca58f8
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2015-12-07 15:10:05 +0100

    edit user
    relates to #11

--- app/admin/users/edit/route.js
+++ app/admin/users/edit/route.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return this.store.ajax({
+      url: `/api/v1.0/users/`
+    }).then(users => {
+      return users.findBy('id', parseInt(params.user_id, 10));
+    });
+  },
+
+  actions: {
+    save() {
+      var user = this.controller.get('model');
+      this.store.ajax({
+        url: `/api/v1.0/users/${user.id}`,
+        method: 'PUT',
+        data: user
+      }).then(() => {
+        this.transitionTo('admin.users');
+      });
+    }
+  }
 });
