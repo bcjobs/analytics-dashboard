ebe61ab4bebae63c75253c786befe91dcd01c7b4
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2015-12-15 20:52:17 +0100

    remove hack that fixes api bug
    relates to #20

--- app/dashboard/route.js
+++ app/dashboard/route.js
@@ -5,9 +5,6 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.ajax({
       url: '/api/v1.0/reports/range'
-    }).then(range => {
-      range.endsAt = '2015-12-13T00:00:00';
-      return range;
     });
   }
 });
