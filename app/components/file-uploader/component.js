17eb14c8ffdb3ff81e7bbe9b68d7a0f06dfa527e
Johnny Oshika <johnnyoshika@gmail.com>
2015-12-23 10:42:10 -0800

    Add dedupe file upload.

---
+++ app/components/file-uploader/component.js
@@ -0,0 +1,33 @@
+import Ember from 'ember';
+import EmberUploader from 'ember-uploader';
+
+export default EmberUploader.FileField.extend({
+
+  filesDidChange(files) {
+    if (Ember.isEmpty(files)) {
+      return;
+    }
+
+    var uploader = EmberUploader.Uploader.create({
+      url: this.get('url'),
+      type: this.get('method') ? this.get('method') : 'POST'
+    });
+
+    uploader.on('progress', this.progress.bind(this, files[0].name));
+    uploader.on('didUpload', this.didUpload.bind(this, files[0].name));
+    uploader.on('didError', this.didError.bind(this, files[0].name));
+    uploader.upload(files[0]);
+  },
+
+  progress(fileName, e) {
+    this.sendAction('onProgress', fileName, e.percent);
+  },
+
+  didUpload(fileName) {
+    this.sendAction('onSuccess', fileName);
+  },
+
+  didError(fileName, jqXHR, textStatus, errorThrown) {
+    this.sendAction('onError', fileName, jqXHR.responseJSON.message);
+  }
+});
