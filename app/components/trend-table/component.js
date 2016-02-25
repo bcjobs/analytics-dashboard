895395027d14b80e7003b4b56c3a3ddf3b0b8bcf
Logan <logan.hollins1@gmail.com>
2016-02-25 14:56:58 -0800

    add csv download
    resolve #36

--- app/components/trend-table/component.js
+++ app/components/trend-table/component.js
@@ -1,12 +1,25 @@
 import Ember from 'ember';
 import ShowMore from 'analytics-dashboard/mixins/show-more';
+import moment from 'npm:moment';
+
+var DATE_FORMAT = 'YYYY-MM-DD';
 
 export default Ember.Component.extend(ShowMore, {
   items: Ember.computed.alias('model'),
-
+  loggedIn: function(){
+    this.get('parentView.targetObject.store').ajax({
+      url: '/api/v1.0/users/me'
+    }).then(function(){
+      this.set('session.loggedIn', true && this.get('exportSet') !== undefined); // prevents download link from appearing if there is no endpoint
+    }.bind(this));
+  },
   actions: {
     showMore() {
+      this.loggedIn();
       this.set('isShowingAll', true);
+    },
+    downloadCSV(){
+      window.location.href = `/api/v1.0/exports/${this.get('exportSet')}?at=${moment(this.get('atDate')).format(DATE_FORMAT)}&type=${this.get('type')}`;
     }
   }
 });
