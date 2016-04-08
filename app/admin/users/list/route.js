496764c0c8243631af39f06513c9a128c48b473e
Logan <logan.hollins1@gmail.com>
2016-04-08 11:32:40 -0700

    Add confirmation dialog to delete user
    Closes bcjobs/analytics#174

--- app/admin/users/list/route.js
+++ app/admin/users/list/route.js
@@ -8,12 +8,23 @@ export default Ember.Route.extend({
   },
 
   actions: {
-    deleteUser(user) {
+    deleteDialog(user){
+      $('.modal').modal();
+      this.set('controller.user', user);
+    },
+    confirmDelete(){
+      let user = this.get('controller.user');
       this.store.ajax({
         url: `/api/v1.0/users/${user.id}`,
         method: 'DELETE'
+      }).then(() =>{
+        this.get('notify').info('User successfully deleted.');
+        this.controller.get('model').removeObject(user);
+        $('.modal').modal('hide');
+      }).catch(err => {
+        var errorMessage = Ember.get(err, 'responseJSON.message') || 'Unable to delete user.';
+        this.get('notify').alert(errorMessage);
       });
-      this.controller.get('model').removeObject(user);
     }
   }
 });
