6f876c34819b64fb5b248358548e11926d257299
Logan <Logan.hollins1@gmail.com>
2016-04-28 12:59:37 -0700

    Add report link, login link, logout link to homepage
    Closes bcjobs/analytics#207

--- app/components/bcic-report/component.js
+++ app/components/bcic-report/component.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 import moment from 'npm:moment';
 
+var DATE_FORMAT = 'YYYY-MM-DD';
+
 function direction(current, average) {
   if (current > average) {
     return 'up';
@@ -33,11 +35,30 @@ export default Ember.Component.extend({
   newJobsTotal: Ember.computed.or('model.charts.monthly.newJobsTotal', 'model.charts.montly.newJobTotal'),
   openJobsAverage: Ember.computed.or('model.charts.monthly.openJobsAverage', 'model.charts.montly.openJobsAverage'),
 
+  at: Ember.computed('atDate', function(){
+    return this.get('atDate') ? moment(this.get('atDate')).format(DATE_FORMAT) : '';
+  }),
+
   startsAt: Ember.computed('range', function() {
     return moment(this.get('range.startsAt')).toDate();
   }),
 
   endsAt: Ember.computed('range', function() {
     return moment(this.get('range.endsAt')).toDate();
-  })
+  }),
+
+  isSoftwareReport: Ember.computed('type', function(){
+    return this.get('type') === 'Software';
+  }),
+
+  actions: {
+    logout() {
+      this.get('targetObject.store').ajax({
+        url: '/api/v1.0/authentication/login',
+        method: 'DELETE'
+      }).then(() => {
+        this.get('session').set('user', null);
+      });
+    }
+  }
 });
