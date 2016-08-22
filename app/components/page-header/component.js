61c5a9b65131a6027f18378f7a899951399582a7
Logan <Logan.hollins1@gmail.com>
2016-08-22 16:34:40 -0700

    Add sector list

--- app/components/page-header/component.js
+++ app/components/page-header/component.js
@@ -11,6 +11,17 @@ export default Ember.Component.extend({
     switchReportName: Ember.computed('type', function(){
       return this.get('type') === 'Software' ? "IT Job Report" : "Software Job Report";
     }),
+
+    subsectorList : ["Clean Tech", "Digital Media", "Life Sciences", "Engineering", "ICT", "Other", "Other Tech Sector",
+     "Other Apparel", "Other Auction", "Other Automotive", "Other Construction", "Other Consulting",
+     "Other Consumer Services", "Other Education", "Other Energy", "Other Engineering Sector", "Other Entertainment",
+      "Other Environment", "Other Financial Services", "Other Government", "Other Health", "Other Law", "Other Marketing",
+      "Other Real Estate", "Other Retail", "Other Staffing", "Other Supply Chain"],
+
+    sectorList : ["Technology", "Other", "Apparel", "Auction", "Automotive", "Construction", "Consulting", "Consumer Services",
+     "Education", "Energy", "Engineering Sector", "Entertainment", "Environment", "Financial Services",
+     "Government", "Health", "Law", "Marketing", "Real Estate", "Retail", "Staffing", "Supply Chain"],
+
   actions: {
     switchReport(currentReport){
       this.sendAction('action', currentReport, this.get('at'));
