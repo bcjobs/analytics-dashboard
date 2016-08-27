4e78ea0a555f728895bc77b0d7c55f00ceb322eb
Logan <Logan.hollins1@gmail.com>
2016-08-26 20:24:20 -0700

    Load sector list from api

--- app/components/page-header/component.js
+++ app/components/page-header/component.js
@@ -4,6 +4,15 @@ import moment from 'npm:moment';
 var DATE_FORMAT = 'YYYY-MM-DD';
 
 export default Ember.Component.extend({
+    init(){
+      this._super();
+      this.get('parentView.targetObject.store').ajax({
+        url: `/api/v1.0/sectors`
+      }).then((sectorList) => {
+        this.set('sectorList', sectorList);
+      });
+    },
+
     at: Ember.computed('atDate', function(){
       return this.get('atDate') ? moment(this.get('atDate')).format(DATE_FORMAT) : '';
     }),
@@ -12,199 +21,7 @@ export default Ember.Component.extend({
       return this.get('type') === 'Software' ? "IT Job Report" : "Software Job Report";
     }),
 
-    sectorList: [
-  {
-    "name":"Technology",
-    "subSectors":[
-      {
-        "name":"Clean Tech"
-      },
-      {
-        "name":"Digital Media"
-      },
-      {
-        "name":"Life Sciences"
-      },
-      {
-        "name":"Engineering"
-      },
-      {
-        "name":"ICT"
-      },
-      {
-        "name":"Other Tech Sector"
-      }
-    ]
-  },
-  {
-    "name":"Other",
-    "subSectors":[
-      {
-        "name":"Other"
-      }
-    ]
-  },
-  {
-    "name":"Apparel",
-    "subSectors":[
-      {
-        "name":"Other Apparel"
-      }
-    ]
-  },
-  {
-    "name":"Auction",
-    "subSectors":[
-      {
-        "name":"Other Auction"
-      }
-    ]
-  },
-  {
-    "name":"Automotive",
-    "subSectors":[
-      {
-        "name":"Other Automotive"
-      }
-    ]
-  },
-  {
-    "name":"Construction",
-    "subSectors":[
-      {
-        "name":"Other Construction"
-      }
-    ]
-  },
-  {
-    "name":"Consulting",
-    "subSectors":[
-      {
-        "name":"Other Consulting"
-      }
-    ]
-  },
-  {
-    "name":"Consumer Services",
-    "subSectors":[
-      {
-        "name":"Other Consumer Services"
-      }
-    ]
-  },
-  {
-    "name":"Education",
-    "subSectors":[
-      {
-        "name":"Other Education"
-      }
-    ]
-  },
-  {
-    "name":"Energy",
-    "subSectors":[
-      {
-        "name":"Other Energy"
-      }
-    ]
-  },
-  {
-    "name":"Engineering Sector",
-    "subSectors":[
-      {
-        "name":"Other Engineering Sector"
-      }
-    ]
-  },
-  {
-    "name":"Entertainment",
-    "subSectors":[
-      {
-        "name":"Other Entertainment"
-      }
-    ]
-  },
-  {
-    "name":"Environment",
-    "subSectors":[
-      {
-        "name":"Other Environment"
-      }
-    ]
-  },
-  {
-    "name":"Financial Services",
-    "subSectors":[
-      {
-        "name":"Other Financial Services"
-      }
-    ]
-  },
-  {
-    "name":"Government",
-    "subSectors":[
-      {
-        "name":"Other Government"
-      }
-    ]
-  },
-  {
-    "name":"Health",
-    "subSectors":[
-      {
-        "name":"Other Health"
-      }
-    ]
-  },
-  {
-    "name":"Law",
-    "subSectors":[
-      {
-        "name":"Other Law"
-      }
-    ]
-  },
-  {
-    "name":"Marketing",
-    "subSectors":[
-      {
-        "name":"Other Marketing"
-      }
-    ]
-  },
-  {
-    "name":"Real Estate",
-    "subSectors":[
-      {
-        "name":"Other Real Estate"
-      }
-    ]
-  },
-  {
-    "name":"Retail",
-    "subSectors":[
-      {
-        "name":"Other Retail"
-      }
-    ]
-  },
-  {
-    "name":"Staffing",
-    "subSectors":[
-      {
-        "name":"Other Staffing"
-      }
-    ]
-  },
-  {
-    "name":"Supply Chain",
-    "subSectors":[
-      {
-        "name":"Other Supply Chain"
-      }
-    ]
-  }
-],
+    sectorList: null,
 
   actions: {
     switchReport(currentReport){
